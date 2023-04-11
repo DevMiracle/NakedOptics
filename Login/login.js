1b5c152a7ea40497eac533a9f23e674e469f1fb6
DevMiracle <dev.miracle20@gmail.com>
2023-04-11 16:35:09 +0400

    commit1

---
+++ Login/login.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', function() {
+  this.classList.toggle('active');
+});
+
+
+function checkPassword() {
+    event.preventDefault(); // Остановить отправку формы
+  let password = document.getElementById("psw").value;
+  let error = document.getElementById("error");
+
+  if (password !== "mypassword") {
+    document.getElementById("error").style.display = "block";
+    document.getElementById("psw").style.border = "0.5px solid red";
+  } 
+  else {
+    error.style.display = "none";
+  }
+}
+
+const passwordInput = document.getElementById("psw");
+const togglePasswordButton = document.getElementById("togglePassword");
+const eyeIcon = document.getElementById("eye-icon");
+
+togglePasswordButton.addEventListener("click", function () {
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+  eyeIcon.setAttribute("src", type === "password" ? "Files/vectorShow.svg" : "Files/vectorHide.svg");
+});
+
