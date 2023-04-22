a322a21c1b4ce55c6fe732973f31d83709c7252b
DevMiracle <127613713+DevMiracle@users.noreply.github.com>
2023-04-22 16:28:33 +0400

    Delete Optics directory

--- Optics/optics.js
+++
@@ -1,8 +0,0 @@
-let burgerMenu = document.getElementById('burger-menu');
-
-let overlay = document.getElementById('burger-menu-list');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
